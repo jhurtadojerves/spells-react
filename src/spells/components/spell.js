61f02c1d9305b2dc0750afe65e6b549e0226528a
Julio Hurtadoe <juliohurtado.backend@gmail.com>
2018-06-01 22:06:54 -0500

    Fix spell name

--- src/spells/components/spell.js
+++ src/spells/components/spell.js
@@ -4,9 +4,9 @@ import './spell.css'
 function Spell(props) {
   return(
     <article className='Spell'>
-      <h2 className='Spell-title'><a href="#">{ props.title }</a></h2>
-      <span className='Spell-type'>{ props.type }</span>
-      <span className='Spell-type'>{ props.method }</span>
+      <h2 className='Spell-title'><a href="#">{ props.name }</a></h2>
+      <span className='Spell-type'>{ props.type } </span>
+      <span className='Spell-type'>{ props.method } </span>
       <span className='Spell-type'>{ props.object }</span>
       <p className='Spell-description'>{ props.description }</p>
     </article>
