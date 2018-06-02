41ec1093babd284eed3f4ff5ac7786d1de886c0d
Julio Hurtadoe <juliohurtado.backend@gmail.com>
2018-06-02 14:31:31 -0500

    Fix API

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import HomeLayout from '../components/home-layout'
 import Spells from '../../spells/components/spells'
+import Paginator from '../../spells/components/paginator'
 
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      spells: []
-    }
+class Home extends Component{
+  state = {
+    spells: [],
+    link: 'https://hechizos.herokuapp.com/api/spells/?format=json&page=1',
+    previous: '',
+    next: ''
   }
 
   componentWillMount() {
-    fetch('https://hechizos.herokuapp.com/api/spells/?format=json')
+    fetch(this.state.link)
       .then(response => response.json())
       .then(spells => {
-        spells.forEach(spell => {
+        spells.results.forEach(spell => {
           let data = {
             name: spell.name,
             type: spell.type,
@@ -22,14 +23,34 @@ class Home extends Component {
             object: spell.object,
             description: spell.description
           }
-          this.setState({spells: this.state.spells.concat([data])})
+          this.setState({
+            spells: this.state.spells.concat([data]),
+            link: this.state.link,
+            previous: spells.previous,
+            next: spells.next
+          })
         })
       })
   }
+  previousLink = () => {
+    this.setState({
+      spells: this.state.spells,
+      link: this.state.previous,
+    })
+  }
+  nextLink = () =>{
+    this.setState({
+      spells: this.state.spells,
+      link: this.state.next,
+    })
+  }
+
   render() {
     return (
       <HomeLayout>
+        
         <Spells spells={this.state.spells} />
+
       </HomeLayout>
     )
   }
---
+++ src/spells/components/paginator.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './paginator.css'
+
+function Paginator(props) {
+  return (
+    <div className='Pager'
+      onClick={ props.handleClick() }
+    >
+      <a className="Pagination-button" href="#">{ props.text }</a>
+    </div>
+  )
+}
+
+export default Paginator
+
